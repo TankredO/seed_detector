952007f75601825b9872b05b1e5ff35df4572cbf
Tankred Ott <ott.tankred@gmail.com>
2022-04-14 11:27:50 +0200

    GUI tests

--- seed_detector/gui/__init__.py
+++ seed_detector/gui/__init__.py
@@ -3,30 +3,50 @@ from tkinter import filedialog
 
 from PIL import Image, ImageTk
 
+import numpy as np
 
-def openfn():
+
+def open_function():
     filename = filedialog.askopenfilename(title='open')
     return filename
-
-
-def open_img():
-    x = openfn()
-    img = Image.open(x)
-    img = img.resize((250, 250), Image.ANTIALIAS)
-    img = ImageTk.PhotoImage(img)
-    panel = tk.Label(image=img)
-    panel.image = img
-    panel.pack()
 
 
 class GUIApp:
     def __init__(self):
         self.window = tk.Tk()
+        self.window.title('Seed Detector')
+        self.window.geometry('800x600')
         label = tk.Label(text="Python rocks!")
         label.pack()
 
-        btn = tk.Button(self.window, text='open image', command=open_img)
+        btn = tk.Button(self.window, text='open image', command=self.open_image)
         btn.pack()
+
+        self.image_label = tk.Label()
+        self.image_label.pack()
+
+        btn2 = tk.Button(self.window, text='segment', command=self.segment)
+        btn2.pack()
+
+        self.image = None
+
+    def open_image(self):
+        filename = open_function()
+        img = Image.open(filename)
+        self.image = np.asanyarray(img.copy())
+        img = img.resize((250, 250), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(img)
+        if self.image_label is None:
+            panel = tk.Label(image=img)
+            panel.image = img
+            panel.pack()
+            self.image_label = panel
+        else:
+            self.image_label.configure(image=img)
+            self.image_label.image = img
+
+    def segment(self):
+        print(self.image)
 
     def run(self):
         self.window.mainloop()
